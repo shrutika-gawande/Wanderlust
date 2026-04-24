463d76077b02e2250d2fb852a658debe2d9e4a0b
shrutika-gawande <shrutikagawande80@gmail.com>
2026-04-24 23:26:44 +0530

    implemented routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Homepage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Destinations from './pages/DestinationsPage'
+import Packages from './pages/PackagesPage'
+import Contact from './pages/ContactPage'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/destinations" element={<Destinations />} />
+        <Route path="/packages" element={<Packages />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
