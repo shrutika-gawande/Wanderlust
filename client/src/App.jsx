7b9dd0a7e8f2b3c56d567592e77196f9c2e9274b
shrutika-gawande <shrutikagawande80@gmail.com>
2026-04-29 23:56:52 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Toaster } from 'react-hot-toast';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import Homepage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Destinations from './pages/DestinationsPage'
-import Packages from './pages/PackagesPage'
-import Contact from './pages/ContactPage'
+import DestinationsPage from './pages/DestinationsPage'
+import PackagesPage from './pages/PackagesPage'
+import ContactPage from './pages/ContactPage';
 
 export default function App() {
   return (
@@ -15,10 +17,11 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<AboutPage />} />
-        <Route path="/destinations" element={<Destinations />} />
-        <Route path="/packages" element={<Packages />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/destinations" element={<DestinationsPage />} />
+        <Route path="/packages" element={<PackagesPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
+      <Toaster position="bottom-right" />
       <Footer />
     </BrowserRouter>
   )
